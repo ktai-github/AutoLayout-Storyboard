7a07631b497623b73e13fa32330cd39a9485f0cb
Kevin T <kevin.tai.mail@gmail.com>
2018-02-20 19:08:47 -0800

    all shapes added

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout-Storyboard
+//
+//  Created by KevinT on 2018-02-20.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout-Storyboard
+//
+//  Created by KevinT on 2018-02-20.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
